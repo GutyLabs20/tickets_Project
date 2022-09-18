09acb2045c6fbea11ddeaf13b1e65b9fc7bbee5e
GutyLabs <gutylabs20@gmail.com>
2022-09-18 15:27:51 -0500

    sin vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,12 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <script src="{{ mix('js/app.js') }}" defer></script>
         <!-- Styles -->
         @livewireStyles
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
