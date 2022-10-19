6c4b5c57f8339578843648d54a4333393bac98a8
GutyLabs <gutylabs20@gmail.com>
2022-10-19 18:58:37 -0500

    modelos utilitarios

--- app/Http/Livewire/Utilitarios/CategoriasIndex.php
+++ app/Http/Livewire/Utilitarios/CategoriasIndex.php
@@ -3,9 +3,29 @@
 namespace App\Http\Livewire\Utilitarios;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class CategoriasIndex extends Component
 {
+    use WithPagination;
+
+    public $title;
+    public $modal_edit = false;
+    public $tipo, $nombre, $descripcion;
+
+    protected $listeners = ['render'];
+
+    protected $rules = [
+        'tipo.nombre' => 'required|string|min:2',
+        'tipo.descripcion' => 'required|string|min:2',
+        'tipo.slug' => 'string'
+    ];
+
+    public function mount()
+    {
+        $this->title = "Categorías";
+    }
+
     public function render()
     {
         return view('livewire.utilitarios.categorias-index');
