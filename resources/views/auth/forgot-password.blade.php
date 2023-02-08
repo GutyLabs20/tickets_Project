88d1e318a0f0932e74813761340e6d19866d9834
GutyLabs <gutylabs20@gmail.com>
2023-02-08 11:13:46 -0500

    login optimize

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -20,13 +20,13 @@
             @csrf
 
             <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-label for="email" value="{{ __('E-Mail') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+            <div class="mt-4">
+                <x-jet-button class="mt-1 w-full flex items-center justify-center text-center">
+                    {{ __('Password Reset') }}
                 </x-jet-button>
             </div>
         </form>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,32 +17,104 @@
 
             <div>
                 <x-jet-label for="email" value="{{ __('E-Mail') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required autofocus />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
+            {{-- <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember Me') }}</span>
                 </label>
+            </div> --}}
+
+            <div class="mt-4">
+                <x-jet-button class="mt-1 w-full flex items-center justify-center text-center">
+                    {{ __('Login') }}
+                </x-jet-button>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-center mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                        href="{{ route('password.request') }}">
                         {{ __('Forgot Your Password?') }}
                     </a>
                 @endif
 
-                <x-jet-button class="ml-4">
+                {{-- <x-jet-button class="ml-4">
                     {{ __('Login') }}
-                </x-jet-button>
+                </x-jet-button> --}}
+
             </div>
         </form>
+
+
+        {{-- <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div class="w-full max-w-md space-y-8">
+                <div>
+                    <img class="mx-auto h-12 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+                    <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                        {{ __('Sign in to your account') }}</h2>
+                </div>
+                <form class="mt-8 space-y-6" action="{{ route('login') }}" method="POST">
+
+                    @csrf
+
+                    <input type="hidden" name="remember" value="true">
+                    <div class="-space-y-px rounded-md shadow-sm">
+                        <div>
+                            <label for="email-address" class="sr-only">{{ __('E-Mail') }}</label>
+                            <input id="email-address" name="email" type="email" autocomplete="email" required autofocus :value="old('email')"
+                                class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                placeholder="{{ __('E-Mail') }}">
+                        </div>
+                        <div>
+                            <label for="password" class="sr-only">{{ __('Password') }}</label>
+                            <input id="password" name="password" type="password" autocomplete="current-password"
+                                required
+                                class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                placeholder="{{ __('Password') }}">
+                        </div>
+                    </div>
+
+                    <div>
+                        <button type="submit"
+                            class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                                <!-- Heroicon name: mini/lock-closed -->
+                                <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                    aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                        d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </span>
+                            {{ __('Login') }}
+                        </button>
+                    </div>
+
+                    <div class="flex items-center justify-center">
+                        @if (Route::has('password.request'))
+                            <div class="text-sm">
+                                <a href="{{ route('password.request') }}"
+                                    class="font-medium text-center text-indigo-600 hover:text-indigo-500">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            </div>
+                        @endif
+                    </div>
+                </form>
+            </div>
+        </div> --}}
+
     </x-jet-authentication-card>
 </x-guest-layout>
